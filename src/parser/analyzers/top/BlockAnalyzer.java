ccd5c9c5d9e0c58be1427e7fc9643ddd10241a55
HackLordMonster062 <hackmonster062@gmail.com>
2024-12-21 20:43:48 +0200

    Started block analyzer

--- src/parser/analyzers/top/BlockAnalyzer.java
+++ src/parser/analyzers/top/BlockAnalyzer.java
@@ -1,13 +1,30 @@
 package parser.analyzers.top;
 
+import lexer.token.TokenType;
 import parser.Parser;
 import parser.analyzers.inline.ExpressionAnalyzer;
 import parser.nodes.ASTNode;
+import parser.nodes.components.BlockNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockAnalyzer {
-    public static ASTNode parse(final Parser parser) {
+    public static BlockNode parse(final Parser parser) {
         // TODO
 
-        return ExpressionAnalyzer.parse(parser);
+        List<ASTNode> nodes = new ArrayList<>();
+
+        ASTNode node;
+
+        while (!parser.check(TokenType.CLOSE_BRACES)) {
+            node = ExpressionAnalyzer.parse(parser);
+            if (node == null) node = FunctionDeclarationAnalyzer.parse(parser);
+
+            if (node != null)
+                nodes.add(node);
+        }
+
+        return new BlockNode(nodes);
     }
 }
