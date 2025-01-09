45e481b0873a64f3f842d392330212d2a78ae924
dvir <dvirm.biton@gmail.com>
2025-01-09 15:58:39 +0200

    Changed class node

--- src/parser/nodes/classes/ClassDeclarationNode.java
+++ src/parser/nodes/classes/ClassDeclarationNode.java
@@ -2,15 +2,20 @@ package parser.nodes.classes;
 
 import parser.nodes.ASTNode;
 import parser.nodes.FunctionDeclarationNode;
+import parser.nodes.components.ArgumentNode;
+import parser.nodes.components.BlockNode;
+import parser.nodes.components.ParameterNode;
 
 import java.util.List;
 
 public record ClassDeclarationNode(
     String name,
-    List<String> modifier,
+    List<ParameterNode> primaryConstructor,
     String baseClass,
+    List<ArgumentNode> baseClassArguments,
     List<String> interfaces,
     List<FieldNode> fields,
     List<FunctionDeclarationNode> methods,
-    List<ConstructorNode> constructors
+    List<ConstructorNode> constructors,
+    BlockNode initBlock
 ) implements ASTNode {}
