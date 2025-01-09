b8c2d7b0ce5dacc906257a299a5eae19fd360b50
dvir <dvirm.biton@gmail.com>
2025-01-09 15:58:39 +0200

    Added symbol cohesion check (only for functions)

--- src/semantic_analysis/SymbolCohesionCheck.java
+++ src/semantic_analysis/SymbolCohesionCheck.java
@@ -4,9 +4,9 @@ import parser.nodes.ASTNode;
 import parser.nodes.FunctionCall;
 import parser.nodes.FunctionDeclarationNode;
 import parser.nodes.components.BlockNode;
-import parser.nodes.variable.VariableAssignment;
-import parser.nodes.variable.VariableDeclaration;
-import parser.nodes.variable.VariableReference;
+import parser.nodes.variable.VariableAssignmentNode;
+import parser.nodes.variable.VariableDeclarationNode;
+import parser.nodes.variable.VariableReferenceNode;
 import semantic_analysis.exceptions.SA_UnresolvedSymbolException;
 
 import java.util.ArrayList;
@@ -15,41 +15,41 @@ import java.util.Objects;
 
 public class SymbolCohesionCheck {
     public static List<FunctionDeclarationNode> performCheck(BlockNode root) throws SA_UnresolvedSymbolException {
-        List<ASTNode> statements = root.getChildren();
+        List<ASTNode> statements = root.children();
 
-        List<VariableDeclaration> variables = new ArrayList<>();
+        List<VariableDeclarationNode> variables = new ArrayList<>();
 
         List<FunctionDeclarationNode> functions = loadFunctions(root);
 
         for (ASTNode statement : statements) {
             if (statement instanceof FunctionCall functionCall) {
                 if (functions.stream().noneMatch(
-                        function -> Objects.equals(function.getName(), functionCall.getName())
+                        function -> Objects.equals(function.name(), functionCall.name())
                 )) {
-                    throw new SA_UnresolvedSymbolException("Function '" + functionCall.getName() + "' does not exist in the current context");
+                    throw new SA_UnresolvedSymbolException("Function '" + functionCall.name() + "' does not exist in the current context");
                 }
             }
-            if (statement instanceof VariableDeclaration declaration) {
+            if (statement instanceof VariableDeclarationNode declaration) {
                 if (variables.stream().anyMatch(
-                        variable -> Objects.equals(variable.getName(), declaration.getName())
+                        variable -> Objects.equals(variable.name(), declaration.name())
                 )) {
-                    throw new SA_UnresolvedSymbolException("Variable '" + declaration.getName() + "' already exists in the current context");
+                    throw new SA_UnresolvedSymbolException("Variable '" + declaration.name() + "' already exists in the current context");
                 }
 
                 variables.add(declaration);
             }
-            if (statement instanceof VariableReference reference) {
+            if (statement instanceof VariableReferenceNode reference) {
                 if (variables.stream().noneMatch(
-                        variable -> Objects.equals(variable.getName(), reference.getVariable())
+                        variable -> Objects.equals(variable.name(), reference.variable())
                 )) {
-                    throw new SA_UnresolvedSymbolException("Variable '" + reference.getVariable() + "' does not exist in the current context");
+                    throw new SA_UnresolvedSymbolException("Variable '" + reference.variable() + "' does not exist in the current context");
                 }
             }
-            if (statement instanceof VariableAssignment reference) {
+            if (statement instanceof VariableAssignmentNode reference) {
                 if (variables.stream().noneMatch(
-                        variable -> Objects.equals(variable.getName(), reference.getVariable())
+                        variable -> Objects.equals(variable.name(), reference.variable())
                 )) {
-                    throw new SA_UnresolvedSymbolException("Variable '" + reference.getVariable() + "' does not exist in the current context");
+                    throw new SA_UnresolvedSymbolException("Variable '" + reference.variable() + "' does not exist in the current context");
                 }
             }
         }
@@ -58,7 +58,7 @@ public class SymbolCohesionCheck {
     }
 
     public static List<FunctionDeclarationNode> loadFunctions(BlockNode root) {
-        List<ASTNode> statements = root.getChildren();
+        List<ASTNode> statements = root.children();
         List<FunctionDeclarationNode> functions = new ArrayList<>();
 
         for (ASTNode statement : statements) {
