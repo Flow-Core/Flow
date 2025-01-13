a776323073d1c4dee5ca69406d1aa955a97e7dc1
fylora <fylora189@gmail.com>
2025-01-13 16:40:12 +0200

    Added binary constructor

--- src/parser/nodes/BinaryExpressionNode.java
+++ src/parser/nodes/BinaryExpressionNode.java
@@ -4,4 +4,10 @@ public class BinaryExpressionNode implements ExpressionNode {
     public ExpressionNode left;
     public ExpressionNode right;
     public String operator;
+
+    public BinaryExpressionNode(ExpressionNode left, ExpressionNode right, String operator) {
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+    }
 }
