a7ac4e4a122d083b3643ecaf10a8a5a86c827ca0
fylora <fylora189@gmail.com>
2025-01-14 07:18:14 +0200

    Refactored initializedVariable

--- src/parser/nodes/variable/InitializedVariableNode.java
+++ src/parser/nodes/variable/InitializedVariableNode.java
@@ -2,7 +2,12 @@ package parser.nodes.variable;
 
 import parser.nodes.ASTNode;
 
-public record InitializedVariableNode(
-    VariableDeclarationNode declaration,
-    VariableAssignmentNode assignment
-) implements ASTNode {}
+public class InitializedVariableNode implements ASTNode {
+    public VariableDeclarationNode declaration;
+    public VariableAssignmentNode assignment;
+
+    public InitializedVariableNode(VariableDeclarationNode declaration, VariableAssignmentNode assignment) {
+        this.declaration = declaration;
+        this.assignment = assignment;
+    }
+}
