3fccf5efbb5210694d35851abcc30d1762928c99
fylora <fylora189@gmail.com>
2025-01-15 09:00:45 +0200

    Refactored Lexer for readability and performance

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -4,13 +4,11 @@ import lexer.token.Token;
 import lexer.token.TokenType;
 
 import java.util.ArrayList;
-import java.util.EnumMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
-    public static final EnumMap<TokenType, Pattern> patterns = new EnumMap<>(TokenType.class);
     private final List<Token> tokens;
     private final String code;
     private int currentLine;
@@ -55,7 +53,7 @@ public class Lexer {
 
     private Token nextToken() {
         for (final TokenType type : TokenType.values()) {
-            final Pattern pattern = patterns.get(type);
+            final Pattern pattern = Pattern.compile(type.getRegex());
             final Matcher matcher = pattern.matcher(code.substring(currentPosition));
 
             if (matcher.lookingAt()) {
@@ -80,92 +78,5 @@ public class Lexer {
         }
 
         return null;
-    }
-
-    static {
-        patterns.put(TokenType.COMMENT, Pattern.compile("//.*|/\\*[^*]*\\*+(?:[^/*][^*]*\\*+)*/"));
-
-        // Keywords (must be checked before identifiers)
-        patterns.put(TokenType.IF, Pattern.compile("if"));
-        patterns.put(TokenType.ELSE, Pattern.compile("\\belse\\b"));
-        patterns.put(TokenType.FOR, Pattern.compile("\\bfor\\b"));
-        patterns.put(TokenType.FOREACH, Pattern.compile("\\bforeach\\b"));
-        patterns.put(TokenType.WHILE, Pattern.compile("\\bwhile\\b"));
-        patterns.put(TokenType.DO, Pattern.compile("\\do\\b"));
-        patterns.put(TokenType.FUNC, Pattern.compile("\\b(func)\\b"));
-        patterns.put(TokenType.CLASS, Pattern.compile("\\bclass\\b"));
-        patterns.put(TokenType.INTERFACE, Pattern.compile("\\binterface\\b"));
-        patterns.put(TokenType.CONSTRUCTOR, Pattern.compile("\\bconstructor\\b"));
-        patterns.put(TokenType.INIT, Pattern.compile("\\binit\\b"));
-        patterns.put(TokenType.TRY, Pattern.compile("\\btry\\b"));
-        patterns.put(TokenType.CATCH, Pattern.compile("\\bcatch\\b"));
-
-        patterns.put(TokenType.CONST, Pattern.compile("\\bconst\\b"));
-        patterns.put(TokenType.VAL, Pattern.compile("\\bval\\b"));
-        patterns.put(TokenType.VAR, Pattern.compile("\\bvar\\b"));
-
-        patterns.put(TokenType.MODIFIER, Pattern.compile("\\b(private|" +
-            "protected|" +
-            "public|" +
-            "static|" +
-            "abstract|" +
-            "final|" +
-            "open|" +
-            "data|" +
-            "sealed|" +
-            "override" +
-        ")\\b"));
-
-        // Boolean literals
-        patterns.put(TokenType.BOOLEAN, Pattern.compile("\\b(true|false)\\b"));
-
-        // IP
-        patterns.put(TokenType.IPV4, Pattern.compile("^\\d+\\.\\d+\\.\\d+\\.\\d+"));
-        patterns.put(TokenType.IPV6, Pattern.compile(
-                "^(" +
-                        "([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|" +
-                        "([0-9a-fA-F]{1,4}:){1,7}:|" +
-                        "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|" +
-                        "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" +
-                        "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|" +
-                        "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|" +
-                        "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|" +
-                        "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|" +
-                        ":((:[0-9a-fA-F]{1,4}){1,7}|:)|" +
-                        "fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]+|" +
-                        ":((ffff(:0{1,4})?:)?(([0-9]{1,3}\\.){3}[0-9]{1,3}))|" +
-                        "([0-9a-fA-F]{1,4}:){1,4}:(([0-9]{1,3}\\.){3}[0-9]{1,3})" +
-                        ")"
-        ));
-
-        // Numbers
-        patterns.put(TokenType.FLOAT, Pattern.compile("(^\\d+\\.\\d+f|^\\d+f|^\\.\\d+f)"));
-        patterns.put(TokenType.DOUBLE, Pattern.compile("(\\d+\\.\\d+|\\.\\d+)"));
-        patterns.put(TokenType.INT, Pattern.compile("^\\d+"));
-
-        // Identifiers
-        patterns.put(TokenType.IDENTIFIER, Pattern.compile("[a-zA-Z_][a-zA-Z0-9_]*"));
-
-        // Strings
-        patterns.put(TokenType.STRING, Pattern.compile("\"(\\\\.|[^\"])*\""));
-
-        // Operators
-        patterns.put(TokenType.OPERATOR, Pattern.compile("==|!=|<=|>=|<|>|\\+\\+|--|\\+|-|\\*|/|%|&&|\\|\\|"));
-        patterns.put(TokenType.EQUAL_OPERATOR, Pattern.compile("="));
-        patterns.put(TokenType.COLON_OPERATOR, Pattern.compile(":"));
-        patterns.put(TokenType.DOT_OPERATOR, Pattern.compile("\\."));
-        patterns.put(TokenType.COMMA, Pattern.compile(","));
-
-        // Grouping and Bracketing
-        patterns.put(TokenType.OPEN_PARENTHESES, Pattern.compile("\\("));
-        patterns.put(TokenType.CLOSE_PARENTHESES, Pattern.compile("\\)"));
-        patterns.put(TokenType.OPEN_BRACKETS, Pattern.compile("\\["));
-        patterns.put(TokenType.CLOSE_BRACKETS, Pattern.compile("]"));
-        patterns.put(TokenType.OPEN_BRACES, Pattern.compile("\\{"));
-        patterns.put(TokenType.CLOSE_BRACES, Pattern.compile("}"));
-
-        // New Line
-        patterns.put(TokenType.NEW_LINE, Pattern.compile("(\\r\\n|\\n|\\r)"));
-        patterns.put(TokenType.SEMICOLON, Pattern.compile(";"));
     }
 }
--- src/lexer/token/TokenType.java
+++ src/lexer/token/TokenType.java
@@ -1,51 +1,105 @@
 package lexer.token;
 
 public enum TokenType {
-    COMMENT,
+    // Comments
+    COMMENT("//.*|/\\*[^*]*\\*+(?:[^/*][^*]*\\*+)*/"),
 
-    IF,
-    ELSE,
-    FOR,
-    FOREACH,
-    WHILE,
-    DO,
-    FUNC,
-    CLASS,
-    INTERFACE,
-    CONSTRUCTOR,
-    INIT,
-    TRY,
-    CATCH,
+    // Keywords
+    IF("\\bif\\b"),
+    ELSE("\\belse\\b"),
+    FOR("\\bfor\\b"),
+    FOREACH("\\bforeach\\b"),
+    WHILE("\\bwhile\\b"),
+    DO("\\bdo\\b"),
+    FUNC("\\bfunc\\b"),
+    CLASS("\\bclass\\b"),
+    INTERFACE("\\binterface\\b"),
+    TRY("\\btry\\b"),
+    CATCH("\\bcatch\\b"),
 
-    CONST,
-    VAR,
-    VAL,
+    CONST("\\bconst\\b"),
+    VAL("\\bval\\b"),
+    VAR("\\bvar\\b"),
 
-    MODIFIER,
+    MODIFIER("\\b(private|protected|public|static|abstract|final|open|data|sealed|override)\\b"),
 
-    OPERATOR,
-    OPEN_PARENTHESES,
-    CLOSE_PARENTHESES,
-    OPEN_BRACKETS,
-    CLOSE_BRACKETS,
-    OPEN_BRACES,
-    CLOSE_BRACES,
+    PACKAGE("\\bpackage\\b"),
+    IMPORT("\\bimport\\b"),
 
-    IPV6,
-    IPV4,
-    FLOAT,
-    DOUBLE,
-    INT,
-    STRING,
-    BOOLEAN,
+    RETURN("\\breturn\\b"),
+    BREAK("\\bbreak\\b"),
+    CONTINUE("\\bcontinue\\b"),
 
-    IDENTIFIER,
-    EQUAL_OPERATOR,
-    COLON_OPERATOR,
-    DOT_OPERATOR,
-    COMMA,
-    SEMICOLON,
+    IN("\\bin\\b"),
+    IS("\\bis\\b"),
+    AS("\\bas\\b"),
+    IT("\\bit\\b"),
+    NULL("\\bnull\\b"),
 
-    NEW_LINE,
-    EOF
+    SUPER("\\bsuper\\b"),
+    THIS("\\bthis\\b"),
+    INIT("\\binit\\b"),
+    CONSTRUCTOR("\\bconstructor\\b"),
+
+    // Boolean literals
+    BOOLEAN("\\b(true|false)\\b"),
+
+    // IP
+    IPV6("^(" +
+        "([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|" +
+        "([0-9a-fA-F]{1,4}:){1,7}:|" +
+        "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|" +
+        "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" +
+        "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|" +
+        "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|" +
+        "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|" +
+        "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|" +
+        ":((:[0-9a-fA-F]{1,4}){1,7}|:)|" +
+        "fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]+|" +
+        ":((ffff(:0{1,4})?:)?(([0-9]{1,3}\\.){3}[0-9]{1,3}))|" +
+        "([0-9a-fA-F]{1,4}:){1,4}:(([0-9]{1,3}\\.){3}[0-9]{1,3})" +
+        ")"),
+    IPV4("^\\d+\\.\\d+\\.\\d+\\.\\d+"),
+
+    // Numbers
+    FLOAT("(^\\d+\\.\\d+f|^\\d+f|^\\.\\d+f)"),
+    DOUBLE("(\\d+\\.\\d+|\\.\\d+)"),
+    INT("^\\d+"),
+
+    // Identifiers
+    IDENTIFIER("[a-zA-Z_][a-zA-Z0-9_]*"),
+
+    // Strings
+    STRING("\"(\\\\.|[^\"])*\""),
+
+    // Operators
+    OPERATOR("==|!=|<=|>=|<|>|\\+\\+|--|\\+|-|\\*|/|%|&&|\\|\\|"),
+    EQUAL_OPERATOR("="),
+    COLON_OPERATOR(":"),
+    DOT_OPERATOR("\\."),
+    COMMA(","),
+
+    // Grouping and Bracketing
+    OPEN_PARENTHESES("\\("),
+    CLOSE_PARENTHESES("\\)"),
+    OPEN_BRACKETS("\\["),
+    CLOSE_BRACKETS("]"),
+    OPEN_BRACES("\\{"),
+    CLOSE_BRACES("\\}"),
+
+    // New Line
+    NEW_LINE("(\\r\\n|\\n|\\r)"),
+    SEMICOLON(";"),
+
+    EOF(null);
+
+    private final String regex;
+
+    TokenType(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
 }
