ecd4c486091ddd6bad17adc6ca7d7c0b413880d4
dvir <dvirm.biton@gmail.com>
2025-01-28 15:39:06 +0200

    Added getter functions in SymbolTable

--- src/semantic_analysis/SymbolTable.java
+++ src/semantic_analysis/SymbolTable.java
@@ -31,8 +31,6 @@ public record SymbolTable(
     }
 
     public void addToBindingContext(SymbolTable other, String modulePath) {
-        // bindingContext.putAll(other.bindingContext);
-
         other.classes().forEach(classDeclarationNode -> bindingContext.put(classDeclarationNode, joinPath(modulePath, classDeclarationNode.name)));
         other.interfaces().forEach(interfaceNode -> bindingContext.put(interfaceNode, joinPath(modulePath, interfaceNode.name)));
         other.functions().forEach(functionDeclarationNode -> bindingContext.put(functionDeclarationNode, joinPath(modulePath,functionDeclarationNode.name)));
@@ -47,15 +45,39 @@ public record SymbolTable(
         return findInterface(symbol) || findClass(symbol) || findFunction(symbol) || findField(symbol);
     }
 
-    public boolean findInterface(String symbol) {
-        return interfaces().stream().anyMatch(
-            existingInterface -> existingInterface.name.equals(symbol)
-        );
+    public ClassDeclarationNode getClass(String symbol) {
+        return classes().stream().filter(
+            classDeclarationNode -> classDeclarationNode.name.equals(symbol)
+        ).findFirst().orElse(null);
+    }
+
+    public InterfaceNode getInterface(String symbol) {
+        return interfaces().stream().filter(
+            interfaceNode -> interfaceNode.name.equals(symbol)
+        ).findFirst().orElse(null);
+    }
+
+    public FunctionDeclarationNode getFunction(String symbol) {
+        return functions().stream().filter(
+            interfaceNode -> interfaceNode.name.equals(symbol)
+        ).findFirst().orElse(null);
+    }
+
+    public FieldNode getField(String symbol) {
+        return fields().stream().filter(
+            interfaceNode -> interfaceNode.initialization.declaration.name.equals(symbol)
+        ).findFirst().orElse(null);
     }
 
     public boolean findClass(String symbol) {
         return classes().stream().anyMatch(
             existingClass -> existingClass.name.equals(symbol)
+        );
+    }
+
+    public boolean findInterface(String symbol) {
+        return interfaces().stream().anyMatch(
+            existingInterface -> existingInterface.name.equals(symbol)
         );
     }
 
