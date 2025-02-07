533d4b2519d18ff18848d39118cd8c75f5a10b22
HackLordMonster062 <hackmonster062@gmail.com>
2025-02-07 12:55:37 +0200

    Shrunk main

--- src/Main.java
+++ src/Main.java
@@ -16,95 +16,18 @@ public class Main {
         final String file1 = """
             package main.first
             \s
-            func main() {
-                for (x = 10, x < 15, x += 12 * 1)
-                    print(x)
-            \s
-                val x = 10
-                var ip: Ip = 127.0.0.1
-                var d: Double = 124.5215
-                d = 12.9
-                d += 10
-                var x: String? = "test"
-                x?.length
-                x!!.length
-                val i: Float = 12.0f + 24.2f / 2
-                val z = 12f
-                const x: Int = 10 * 2
-                val y: String = "Hello, world!"
-                const multiLine: String = "
-                hello,
-                world!
-                "
-                val d: Int = ip.toAddress().toString().length
-                var NEW: Object = new Object();
-                test(x = 2, 10, y = 8)
-                test2(
-                    x = 2,
-                    10,
-                    8
-                )
-                while (x > 5) {
-                    print("hi!");
-                }
-                if (x == 5) {
-                    print("test")
-                } else if (x > 5) print("hi!")
-                else
-                    print("test2")
-                foreach (x in collection) {
-                }
+            func main(): A? {
+                val x = 2
+                return null
             }
-            func test(x: Int) {
-                val y: C = new C()
-                var x = 10
-                switch (x) {
-                    case (10) {
-                        print(x)
-                    }
-                    case (12) {
-                        print(x + 2)
-                    }
-                    default {
-                        print("error")
-                    }
-                }
-            }
-           \s
-            interface A {
-                func test()
-            }
-           \s
-            open class B: A {
-                init {
-                    print("B created!")
-                }
-                \s
-                override func test() {
-                    print("works!")
-                }
-            }
+            
+            class A {}
            \s""";
 
         final String file2 = """
-        package main.second
-        
-        import main.third.C
-        
-        func test() {
-            val c: C = new C();
-            c.test()
-        }
         """;
 
         final String file3 = """
-        package main.third
-        
-        import main.first.B
-
-        class C: B() {
-        
-        }
         """;
 
         LoggerFacade.initLogger(new ConsoleLogger());
