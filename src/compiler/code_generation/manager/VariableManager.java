339ab448da339351da1853bb030fb1758daa1162
fylora <fylora189@gmail.com>
2025-02-09 18:39:57 +0200

    Added variable manager

---
+++ src/compiler/code_generation/manager/VariableManager.java
@@ -0,0 +1,31 @@
+package compiler.code_generation.manager;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VariableManager {
+    private final Map<String, Integer> variables = new HashMap<>();
+    private final MethodVisitor mv;
+    private int availableCell = 0;
+
+    public VariableManager(MethodVisitor mv) {
+        this.mv = mv;
+    }
+
+    public void declareVariable(String name) {
+        variables.put(name, availableCell);
+        mv.visitVarInsn(Opcodes.ASTORE, availableCell++);
+    }
+
+    public void loadVariable(String name) {
+        if (!variables.containsKey(name)) {
+            throw new IllegalArgumentException("Variable '" + name + "' does not exist");
+        }
+
+        int index = variables.get(name);
+        mv.visitVarInsn(Opcodes.ALOAD, index);
+    }
+}
